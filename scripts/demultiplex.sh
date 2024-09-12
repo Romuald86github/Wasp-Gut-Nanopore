c29a18be91d69aace342953f1b08551a34b1b07e
Romuald86github <123880565+Romuald86github@users.noreply.github.com>
2024-09-12 17:10:49 +0100

    Update demultiplex.sh

--- scripts/demultiplex.sh
+++ scripts/demultiplex.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
 
-basecalled_reads=$1
-output_dir=$2
-
-# Create the output directory if it doesn't exist
-mkdir -p $output_dir
-
-# Demultiplexing using qcat
-# The --threads 4 parameter uses 4 CPU threads for the demultiplexing
-qcat -i $basecalled_reads -o $output_dir --threads 4
